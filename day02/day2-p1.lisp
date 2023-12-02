ad251bbe06cb5eb3e1a7a6eff90bcea192599bab
AugFJTan <fj_tan@hotmail.com>
2023-12-02 17:34:07 +0800

    Add Day 2 Part 1 solution

---
+++ day02/day2-p1.lisp
@@ -0,0 +1,56 @@
+(load "~/.sbclrc")
+(ql:quickload "cl-ppcre")
+
+
+(defun regex-match (str regex)
+    (let ((x (cl-ppcre:all-matches regex str)))
+    (subseq str (car x) (cadr x))))
+
+
+(defun parse-id (str)
+    (parse-integer (regex-match (car (cl-ppcre:split ":" str)) "([0-9]+)")))
+
+
+(defun parse-sets (str)
+    (loop for x in (cl-ppcre:split ";" (cadr (cl-ppcre:split ":" str)))
+     append (list (
+        loop for y in (cl-ppcre:split "," x)
+            collect (list (parse-cube-num y) (parse-cube-colour y))
+        ))))
+
+
+(defun parse-cube-num (str)
+    (parse-integer (regex-match str "([0-9]+)")))
+
+
+(defun parse-cube-colour (str)
+    (regex-match str "(red|green|blue)"))
+
+
+(defun verify-sets (sets)
+    (loop for set in sets
+    append (list
+        (loop for subset in set
+            collect (if (equal (cadr subset) "red")
+                        (if (<= (car subset) 12) 1 0)
+                    (if (equal (cadr subset) "green")
+                        (if (<= (car subset) 13) 1 0)
+                    (if (equal (cadr subset) "blue")
+                        (if (<= (car subset) 14) 1 0))))
+        ))))
+
+
+(defun verify-game (results)
+    (loop for result in results
+    do (if (find 0 result) (return-from verify-game NIL)))
+    1)
+
+
+(defvar n 0)
+(with-open-file (file "input.txt" :direction :input)
+    (loop for line = (read-line file nil)
+        while line
+        do  
+        (if (verify-game (verify-sets (parse-sets line))) (incf n (parse-id line)))
+    ))
+(format t "~a~%" n)  ; Answer: 2528
